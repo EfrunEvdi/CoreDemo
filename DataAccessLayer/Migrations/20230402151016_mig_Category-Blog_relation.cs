16b4da1b72119a13ffafa58631487f0f4fa77c2d
efrunevdi <efruncetkin@gmail.com>
2023-04-02 20:34:15 +0300

    Add project files.

---
+++ DataAccessLayer/Migrations/20230402151016_mig_Category-Blog_relation.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig_CategoryBlog_relation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Blogs_Categories_CategoryID",
+                table: "Blogs");
+
+            migrationBuilder.DropColumn(
+                name: "CategoyID",
+                table: "Blogs");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryID",
+                table: "Blogs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Blogs_Categories_CategoryID",
+                table: "Blogs",
+                column: "CategoryID",
+                principalTable: "Categories",
+                principalColumn: "CategoryID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Blogs_Categories_CategoryID",
+                table: "Blogs");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryID",
+                table: "Blogs",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoyID",
+                table: "Blogs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Blogs_Categories_CategoryID",
+                table: "Blogs",
+                column: "CategoryID",
+                principalTable: "Categories",
+                principalColumn: "CategoryID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
