429c729cbe5131fd72923191531794cf45e5ea61
efrunevdi <efruncetkin@gmail.com>
2023-04-05 02:31:14 +0300

    50. Ders Sonu

--- CoreDemo/Controllers/AboutController.cs
+++ CoreDemo/Controllers/AboutController.cs
@@ -1,9 +1,11 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
+    [AllowAnonymous]
     public class AboutController : Controller
     {
         AboutManager am = new AboutManager(new EfAboutRepository());
--- CoreDemo/Controllers/LoginController.cs
+++ CoreDemo/Controllers/LoginController.cs
@@ -1,12 +1,21 @@
 ﻿using BusinessLayer.Concrete;
 using BusinessLayer.ValidationRules;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using FluentValidation.Results;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace CoreDemo.Controllers
 {
+    [AllowAnonymous]
     public class LoginController : Controller
     {
         WriterManager wm = new WriterManager(new EfWriterRepository());
@@ -44,9 +53,49 @@ namespace CoreDemo.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult SignIn()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(Writer writer)
+        {
+            Context context = new Context();
+            var datavalue = context.Writers.FirstOrDefault(x => x.WriterMail == writer.WriterMail && x.WriterPassword == writer.WriterPassword);
+
+            if (datavalue != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,writer.WriterMail)
+                };
+
+                var useridentity = new ClaimsIdentity(claims, "a");
+                ClaimsPrincipal principal = new ClaimsPrincipal(useridentity);
+                await HttpContext.SignInAsync(principal);
+                return RedirectToAction("Index", "Blog");
+            }
+
+            else
+            {
+                return View();
+            }
+        }
     }
 }
+
+//Context context = new Context();
+//var dataValue = context.Writers.FirstOrDefault(x => x.WriterMail == writer.WriterMail && x.WriterPassword == writer.WriterPassword);
+
+//if (dataValue != null)
+//{
+//    HttpContext.Session.SetString("username", writer.WriterMail);
+//    return RedirectToAction("Index", "Writer");
+//}
+
+//else
+//{
+//    return View();
+//}
